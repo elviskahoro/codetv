74e68282486f3c9078614a6dd871351758161f54
elvis kahoro <github@elvis.ai>
2025-07-23 09:59:49 -0700

    lint: Lint

--- src/firecrawl.py
+++ src/firecrawl.py
@@ -1,7 +1,13 @@
+from msvcrt import kbhit
 from firecrawl.firecrawl import FirecrawlApp
 
+API_KEY: str = "fc-96612b847eab48adaaa9ca9d15fe9d1c"
 
-def firecrawl(api_key: str) -> FirecrawlApp:
+
+def firecrawl(
+    api_key: str,
+    url: str,
+) -> str:
     app: Any = FirecrawlApp(api_key=api_key)
     scrape_data: Dict[str, Any] = app.scrape_url(
         url,
@@ -11,3 +17,5 @@ def firecrawl(api_key: str) -> FirecrawlApp:
         },
     )
     markdown: str = scrape_data["markdown"]
+    print(markdown)
+    return markdown
