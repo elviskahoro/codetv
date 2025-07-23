7d16afc0a4aec5db4590c24cc9c69d87e1112bfb
elvis kahoro <github@elvis.ai>
2025-07-23 09:12:31 -0700

    feat: initial mcp

---
+++ src/mcp.py
@@ -0,0 +1,51 @@
+# trunk-ignore-all(ruff/PGH003,trunk/ignore-does-nothing)
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import modal
+from modal import Image
+from pydantic import BaseModel
+
+
+class GithubSearch(BaseModel):
+    query: str
+    limit: int = 10
+
+
+image: Image = modal.Image.debian_slim().pip_install(
+    "fastapi[standard]",
+    "chalkpy",
+)
+image.add_local_python_source(
+    *[
+        "src",
+    ],
+)
+app = modal.App(
+    name="postman-mcp",
+    image=image,
+)
+
+
+@app.function(
+    region="us-east4",
+    allow_concurrent_inputs=1000,
+    enable_memory_snapshot=False,
+)
+@modal.fastapi_endpoint(
+    method="POST",
+    docs=True,
+)
+def web(
+    webhook: GithubSearch,
+) -> str:
+    return ""
+
+
+@app.local_entrypoint()
+def local(
+    search_query: str,
+) -> None:
+    print("test")
+
